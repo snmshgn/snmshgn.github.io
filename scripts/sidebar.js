3c66796d8ee752096bb94fc885d5208fba6ff31f
Sona Moushigian <snmshgn@aol.com>
2019-02-26 15:50:55 -0500

    Update sidebar.js

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,13 +1,9 @@
 function openNav() {
   document.getElementById("menu").style.width = "720px";
   document.getElementById("menu").style.visibility = "visible";
-  document.getElementByClassName("main").style.marginLeft = "720px";
-  document.body.style.backgroundColor = "rgba(255, 89, 83, 0.8)";
 }
 
 function closeNav() {
   document.getElementById("menu").style.width = "0";
   document.getElementById("menu").style.visibility = "hidden";
-  document.getElementByClassName("main").style.marginLeft= "0";
-  document.body.style.backgroundColor = "white";
 }
