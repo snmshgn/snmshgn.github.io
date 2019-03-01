0f79eda470b82d6a4568943cb68c470600b56147
Sona Moushigian <snmshgn@aol.com>
2019-02-28 20:23:43 -0500

    Update menu JS)

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,18 +1,31 @@
 function openNav() {
-  if (window.matchMedia("(max-width: 760px)").matches) {
-    document.getElementById("menu").style.height = "580px";
-    document.getElementById("menu").style.visibility = "visible";
-  } else {
-    document.getElementById("menu").style.width = "644px";
-    document.getElementById("menu").style.visibility = "visible";
+  function myFunction(x) {
+    if (x.matches) {
+      document.getElementById("menu").style.height = "580px";
+      document.getElementById("menu").style.visibility = "visible";
+    } else {
+      document.getElementById("menu").style.width = "644px";
+      document.getElementById("menu").style.visibility = "visible";
+    }
   }
+
+  var x = window.matchMedia("(max-width: 375px)")
+  myFunction(x)
+  x.addEventListener(myFunction)
 }
+
 function closeNav() {
-  if (window.matchMedia("(max-width: 760px)").matches) {
-    document.getElementById("menu").style.height = "0";
-    document.getElementById("menu").style.visibility = "hidden";
-  } else {
-    document.getElementById("menu").style.width = "0";
-    document.getElementById("menu").style.visibility = "hidden";
+  function myFunction(x) {
+    if (window.matchMedia("(max-width: 760px)").matches) {
+      document.getElementById("menu").style.height = "0";
+      document.getElementById("menu").style.visibility = "hidden";
+    } else {
+      document.getElementById("menu").style.width = "0";
+      document.getElementById("menu").style.visibility = "hidden";
+    }
   }
+
+  var x = window.matchMedia("(max-width: 375px)")
+  myFunction(x)
+  x.addEventListener(myFunction)
 }
