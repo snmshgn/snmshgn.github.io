f3005b7a8ed962642e32779de8a12e5cf5447a61
Sona Moushigian <snmshgn@aol.com>
2019-03-01 18:28:27 -0500

    Semi-final CSS Update

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -5,29 +5,31 @@ function openNav() {
       document.getElementById("menu").style.width = "100%";
       document.getElementById("menu").style.visibility = "visible";
     } else {
-      document.getElementById("menu").style.width = "644px";
+      document.getElementById("menu").style.width = "700px";
       document.getElementById("menu").style.height = "100%";
       document.getElementById("menu").style.visibility = "visible";
     }
   }
 
-  var x = window.matchMedia("(max-width: 375px)")
+  var x = window.matchMedia("(max-width: 1190px)")
   myFunction(x)
   x.addListener(myFunction)
 }
 
 function closeNav() {
   function myFunction(x) {
-    if (window.matchMedia("(max-width: 760px)").matches) {
+    if (x.matches) {
       document.getElementById("menu").style.height = "0";
+      document.getElementById("menu").style.width = "100%";
       document.getElementById("menu").style.visibility = "hidden";
     } else {
       document.getElementById("menu").style.width = "0";
+      document.getElementById("menu").style.height = "100%";
       document.getElementById("menu").style.visibility = "hidden";
     }
   }
 
-  var x = window.matchMedia("(max-width: 375px)")
+  var x = window.matchMedia("(max-width: 1190px)")
   myFunction(x)
   x.addListener(myFunction)
 }
