3c2fad05535c3fbc19f5697dcd4d9ae084355d07
Sona Moushigian <snmshgn@aol.com>
2019-03-15 17:09:17 -0400

    Move nav to top on desktop

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,35 +1,9 @@
 function openNav() {
-  function myFunction(x) {
-    if (x.matches) {
       document.getElementById("menu").style.height = "580px";
-      document.getElementById("menu").style.width = "100vw";
       document.getElementById("menu").style.visibility = "visible";
-    } else {
-      document.getElementById("menu").style.width = "700px";
-      document.getElementById("menu").style.height = "100vh";
-      document.getElementById("menu").style.visibility = "visible";
-    }
-  }
-
-  var x = window.matchMedia("(max-width: 1020px)")
-  myFunction(x)
-  x.addListener(myFunction)
 }
 
 function closeNav() {
-  function myFunction(x) {
-    if (x.matches) {
       document.getElementById("menu").style.height = "0";
-      document.getElementById("menu").style.width = "100vw";
       document.getElementById("menu").style.visibility = "hidden";
-    } else {
-      document.getElementById("menu").style.width = "0";
-      document.getElementById("menu").style.height = "100vh";
-      document.getElementById("menu").style.visibility = "hidden";
-    }
-  }
-
-  var x = window.matchMedia("(max-width: 1020px)")
-  myFunction(x)
-  x.addListener(myFunction)
 }
