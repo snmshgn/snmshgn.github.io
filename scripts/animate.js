5a3e8f4c3f96fdabc074610b241cadf1b8df28ec
Sona Moushigian <snmshgn@aol.com>
2019-03-19 15:22:39 -0400

    Remove need for bottom to be within viewport

--- scripts/animate.js
+++ scripts/animate.js
@@ -2,12 +2,10 @@ $(document).ready(function() {
   // Check if element is scrolled into view
   function isScrolledIntoView(elem) {
     var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
 
     var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
 
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    return ((elemTop >= docViewTop));
   }
   // If element is scrolled into view, fade it in
   $(window).scroll(function() {
