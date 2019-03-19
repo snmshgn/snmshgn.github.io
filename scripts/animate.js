b2de8df4f800cfa577d3d71b1614a5cc65fd4a9e
Sona Moushigian <snmshgn@aol.com>
2019-03-19 15:24:08 -0400

    Move back one commit.

--- scripts/animate.js
+++ scripts/animate.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   // Check if element is scrolled into view
   function isScrolledIntoView(elem) {
     var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
 
     var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
 
-    return ((elemTop >= docViewTop));
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
   }
   // If element is scrolled into view, fade it in
   $(window).scroll(function() {
