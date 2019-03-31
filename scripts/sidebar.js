85ae3dac4e3a2b8b80c67cd1eff8c20591978915
Sona Moushigian <snmshgn@aol.com>
2019-03-30 21:36:02 -0400

    Change menu layout

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,9 +1,11 @@
 function openNav() {
       document.getElementById("menu").style.transform = "translateY(0px)";
       document.getElementById("menu-button").style.visibility = "hidden";
+      document.getElementById("menu").style.visibility = "visible";
 }
 
 function closeNav() {
       document.getElementById("menu").style.transform = "translateY(-580px)";
       document.getElementById("menu-button").style.visibility = "visible";
+      document.getElementById("menu").style.visibility = "hidden";
 }
